f4eeca368753fdff125f9abda0fb2cd3158eaf29
taetaetae <taetaetae_@naver.com>
2020-12-22 12:50:28 +0900

    restdocs-spec-maven-plugin 적용

--- src/test/java/com/taetaetae/springrestdocs/books/BookControllerTest.java
+++ src/test/java/com/taetaetae/springrestdocs/books/BookControllerTest.java
@@ -1,14 +1,13 @@
 package com.taetaetae.springrestdocs.books;
 
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.*;
 import static org.hamcrest.Matchers.*;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import org.junit.jupiter.api.Test;
-import org.omg.IOP.Encoding;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
